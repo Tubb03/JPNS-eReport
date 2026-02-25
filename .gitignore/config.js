078d35c29d76fc83dd50f57e8979401dd80c7b7c
Tubb03 <ch3wienato@gmail.com>
2026-02-25 10:36:07 +0800

    moved firebase config into config,js

---
+++ .gitignore/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCR9EPwlakRDXN1v6Rm19ro7XvowvBnY2k",
+    authDomain: "jpns-ereport.firebaseapp.com",
+    projectId: "jpns-ereport",
+    storageBucket: "jpns-ereport.firebasestorage.app",
+    messagingSenderId: "928703430086",
+    appId: "1:928703430086:web:273fc8a67f22ce1b9e86c7",
+    measurementId: "G-ZD4CJQQ5D2"
+};
